f22474f4f1c1754b489c2ef9f227b270e1007c59
Kanishk Karam <156420103+Kanishkkaram2703@users.noreply.github.com>
2025-11-01 13:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -475,5 +475,5 @@ if __name__ == "__main__":
     print("="*50 + "\n")
     
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+
